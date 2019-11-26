27aae464b31241c97ac508ccdd85dcb0fe6dc8d6
urlchange <salah.karim@e-campus.uab.cat>
2019-11-26 16:45:45 +0800

    reorg linesxd

--- Contract/smartBNB/Contract.cs
+++ Contract/smartBNB/Contract.cs
@@ -9,21 +9,6 @@ namespace smartBNB
     {
         private static readonly byte[] innerPrefix = { 0x01 };
 
-        public static bool Main(string operation, params object[] args)
-        {
-            if (Runtime.Trigger == TriggerType.Application)
-            {
-                switch (operation)
-                {
-                    case "spv":
-                        return Validate((byte[])args[0]);
-                    default:
-                        return false;
-                }
-            }
-            return true;
-        }
-
         // secp256k1 is defined in the ring Z/pZ
         private static readonly BigInteger p = BigInteger.Parse("115792089237316195423570985008687907853269984665640564039457584007908834671663");
         // y^2 = x^3 + 7
@@ -40,57 +25,19 @@ namespace smartBNB
         private static readonly BigInteger x_0 = 0;
         private static readonly BigInteger y_0 = 0;
 
-		private static BigInteger SHA256(byte[] message)
+        public static bool Main(string operation, params object[] args)
         {
-            // Implemented by NEOVM, see bytecode 0xA8 on https://docs.neo.org/developerguide/en/articles/neo_vm.html
-            // https://docs.neo.org/docs/en-us/reference/scapi/fw/dotnet.html
-            return Sha256(message).AsBigInteger();
-        }
-
-        // https://en.wikipedia.org/wiki/Elliptic_curve_point_multiplication#Point_multiplication
-        private static BigInteger[] scalarMultECC(BigInteger x, BigInteger y, BigInteger k)
-        {
-            BigInteger x1, y2;
-            return new BigInteger[2] { x, y };
-        }
-
-        // https://en.wikipedia.org/wiki/Elliptic_Curve_Digital_Signature_Algorithm
-        // inverse_s needs to be calculated by the contract caller
-        private static bool verifyECSDA(BigInteger r, BigInteger s, byte[] message, BigInteger pubkey_x, BigInteger pubkey_y, BigInteger inverse_s)
-        {
-            // Verify public key is valid
-
-            // Verify signature coordinates
-            if (r <= 0 || r >= n || s <= 0 || s >= n){ // r,s \in [1, n-1]
-                return false;
-            }
-            BigInteger e = SHA256(message);
-            BigInteger z = e; // Nothing to be done as e's bit length is 256 == L_n
-            if (((s * inverse_s) % n) != 1)
-            { // Verify inverse_s is correct
-                return false;
-            }
-            BigInteger u1 = (z * inverse_s) % n;
-
-            BigInteger u2 = (r * inverse_s) % n;
-            // The following part (sum of scalar point multiplications) can be optimized further using some math but I chose code correctness over cost
-            BigInteger[] mul1 = scalarMultECC(G_x, G_y, u1);
-
-            BigInteger[] mul2 = scalarMultECC(pubkey_x, pubkey_y, u2);
-
-            BigInteger x1 = mul1[0] + mul2[0];
-            BigInteger y1 = mul1[1] + mul2[1];
-            if (x1 == x_0 && y1 == y_0){ //Check if (x1, y1) == O
-                return false;
-            }
-            if (r == (x1 % n))
+            if (Runtime.Trigger == TriggerType.Application)
             {
-                return true;
+                switch (operation)
+                {
+                    case "spv":
+                        return Validate((byte[])args[0]);
+                    default:
+                        return false;
+                }
             }
-            else
-            {
-                return false;
-            }
+            return true;
         }
 
         private static bool Validate(byte[] proof)
@@ -107,6 +54,55 @@ namespace smartBNB
             if (isValidTx != true)
                 throw new Exception("Proof is not internally consistent");
             return true;
+        }
+
+
+        // https://en.wikipedia.org/wiki/Elliptic_Curve_Digital_Signature_Algorithm
+        // inverse_s needs to be calculated by the contract caller
+        private static bool VerifyECSDA(BigInteger r, BigInteger s, byte[] message, BigInteger pubkey_x, BigInteger pubkey_y, BigInteger inverse_s)
+        {
+            // Verify public key is valid
+
+            // Verify signature coordinates
+            if (r <= 0 || r >= n || s <= 0 || s >= n)
+            { // r,s \in [1, n-1]
+                return false;
+            }
+            BigInteger e = SHA256(message);
+            BigInteger z = e; // Nothing to be done as e's bit length is 256 == L_n
+            if (((s * inverse_s) % n) != 1)
+            { // Verify inverse_s is correct
+                return false;
+            }
+            BigInteger u1 = (z * inverse_s) % n;
+
+            BigInteger u2 = (r * inverse_s) % n;
+            // The following part (sum of scalar point multiplications) can be optimized further using some math but I chose code correctness over cost
+            BigInteger[] mul1 = ScalarMultECC(G_x, G_y, u1);
+
+            BigInteger[] mul2 = ScalarMultECC(pubkey_x, pubkey_y, u2);
+
+            BigInteger x1 = mul1[0] + mul2[0];
+            BigInteger y1 = mul1[1] + mul2[1];
+            if (x1 == x_0 && y1 == y_0)
+            { //Check if (x1, y1) == O
+                return false;
+            }
+            if (r == (x1 % n))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        // https://en.wikipedia.org/wiki/Elliptic_curve_point_multiplication#Point_multiplication
+        private static BigInteger[] ScalarMultECC(BigInteger x, BigInteger y, BigInteger k)
+        {
+            BigInteger x1, y2;
+            return new BigInteger[2] { x, y };
         }
 
         private static bool VerifyTx(byte[] proof)
@@ -132,14 +128,14 @@ namespace smartBNB
             if (txProofTotal <= 0)
                 throw new Exception("Proof total must be positive");
 
-            byte[] computedHash = computeHashFromAunts(txProofIndex, txProofTotal, txProofLeafHash, txProofAunts);
+            byte[] computedHash = ComputeHashFromAunts(txProofIndex, txProofTotal, txProofLeafHash, txProofAunts);
             if (!AreEqual(computedHash, txProofRootHash))
                 throw new Exception("Invalid root hash");
 
             return true;
         }
 
-        private static byte[] computeHashFromAunts(int index, int total, byte[] leafHash, byte[][] innerHashes)
+        private static byte[] ComputeHashFromAunts(int index, int total, byte[] leafHash, byte[][] innerHashes)
         {
             if (index >= total || index < 0 || total <= 0)
                 return null;
@@ -156,34 +152,34 @@ namespace smartBNB
                     if (innerHashes.Length == 0)
                         return null;
 
-                    int numLeft = getSplitPoint(total);
+                    int numLeft = GetSplitPoint(total);
                     if (index < numLeft)
                     {
-                        byte[] leftHash = computeHashFromAunts(index, numLeft, leafHash, TakeArray(innerHashes, innerHashes.Length - 1));
+                        byte[] leftHash = ComputeHashFromAunts(index, numLeft, leafHash, TakeArray(innerHashes, innerHashes.Length - 1));
                         if (leftHash == null)
                         {
                             return null;
                         }
-                        return innerHash(leftHash, innerHashes[innerHashes.Length - 1]);
+                        return InnerHash(leftHash, innerHashes[innerHashes.Length - 1]);
                     }
-                    byte[] rightHash = computeHashFromAunts(index - numLeft, total - numLeft, leafHash, TakeArray(innerHashes, innerHashes.Length - 1));
+                    byte[] rightHash = ComputeHashFromAunts(index - numLeft, total - numLeft, leafHash, TakeArray(innerHashes, innerHashes.Length - 1));
                     if (rightHash == null)
                     {
                         return null;
                     }
-                    return innerHash(innerHashes[innerHashes.Length - 1], rightHash);
+                    return InnerHash(innerHashes[innerHashes.Length - 1], rightHash);
             }
         }
 
         // returns the largest power of 2 less than length
-        private static int getSplitPoint(int length)
+        private static int GetSplitPoint(int length)
         {
             if (length < 1)
                 throw new Exception("Trying to split a tree with size < 1");
 
             uint uLength = (uint)length;
 
-            int bitlen = (int)getBitsCount(uLength);
+            int bitlen = (int)GetBitsCount(uLength);
 
             int k = 1 << (bitlen - 1);
             if (k == length)
@@ -194,7 +190,7 @@ namespace smartBNB
         }
 
         // returns the number of significant bits in n
-        private static uint getBitsCount(uint n)
+        private static uint GetBitsCount(uint n)
         {
             uint count = 0;
             while (n > 0)
@@ -206,7 +202,7 @@ namespace smartBNB
         }
 
         // returns Sha256(0x01 || left || right)
-        private static byte[] innerHash(byte[] left, byte[] right)
+        private static byte[] InnerHash(byte[] left, byte[] right)
         {
             return Sha256(innerPrefix.Concat(left.Concat(right)));
         }
@@ -240,6 +236,13 @@ namespace smartBNB
             }
             return cutted;
         }
-        
+
+        private static BigInteger SHA256(byte[] message)
+        {
+            // Implemented by NEOVM, see bytecode 0xA8 on https://docs.neo.org/developerguide/en/articles/neo_vm.html
+            // https://docs.neo.org/docs/en-us/reference/scapi/fw/dotnet.html
+            return Sha256(message).AsBigInteger();
+        }
+
     }
 }
