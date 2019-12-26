af486599f0b9d598e849a3577e257cbce9d6c04e
Albert <github@albert.sh>
2019-12-26 18:28:08 +0800

    Fix & improve utils

--- utils.py
+++ utils.py
@@ -1,17 +1,21 @@
 def leftpad(a, n):
     return "0"*(n-len(a))+a
 
-def num2byteArray(num):
-    hexed = hex(num)[2:]
+def normalizedHex(n):
+    hexed = hex(n)[2:]
     if(hexed[-1]=="L"):
         hexed=hexed[:-1]
-    hexed=leftpad(hexed, 64)
+    return hexed
+
+def num2byteArray(num):
+    hexed = normalizedHex(num)
+    hexed = leftpad(hexed, 64)
     hexed = [hexed[i:i+2] for i in range(0, len(hexed), 2)]
     hexed.reverse()
     return "{0x"+", 0x".join(hexed)+"}"
 
 def reverseNum(n):
-    h = hex(n)
+    h = normalizedHex(n)
     h = "0"+h if (len(h)%2==1) else h
     hr = [h[i: i+2] for i in range(0, len(h), 2)]
     hr.reverse()
