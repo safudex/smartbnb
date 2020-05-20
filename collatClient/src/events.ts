7514d4b3f5a6c15fc933fee2a820a6f6c06b0d56
Albert <github@albert.sh>
2020-05-20 06:15:26 +0200

    expand events

--- collatClient/src/events.ts
+++ collatClient/src/events.ts
@@ -1,18 +1,32 @@
 import { u } from '@cityofzion/neon-js';
+import { promisify } from "util";
 import redis from 'redis';
 import NeoApi, { NotificationMessage } from './neolib';
 import BncApi from './bnclib';
 
 function neoEventHandler(neo: NeoApi, redisUrl:string, collatId: string, bnc:BncApi) {
   const redisClient = redis.createClient(redisUrl);
-  return (event: NotificationMessage) => {
+  const get:(key:string)=>Promise<string> = promisify(redisClient.get).bind(redisClient); // get("key")
+  const set:(key:string, value:string)=>Promise<"OK"> = promisify(redisClient.set).bind(redisClient); // set("key", "value")
+  return async (event: NotificationMessage) => {
     const operation = u.hexstring2str(event.event[0].value);
-
+    if(operation === 'priceupdated'){
+        const newPrice:number = hex2int(event.event[1].value)
+        const collateral = str2int(await get("collateral"));
+        const collateralizedToken = str2int(await get("collateralizedToken"));
+        // Check
+        return;
+    }
+    const rellevantCollatId = u.hexstring2str(event.event[1].value);
+    if(rellevantCollatId !== collatId){
+        return; // ignore event
+    }
     switch (operation) {
       case 'portrequestcreated':
         //
         break;
       case 'withdrawrequestcreated':
+        bnc.send()
         //
         break;
       case 'challengewithdrawcreated':
@@ -26,6 +40,19 @@ function neoEventHandler(neo: NeoApi, redisUrl:string, collatId: string, bnc:Bnc
         break;
     }
   };
+}
+
+function hex2int(hex:string):number{
+    const reverseHex = (hex:string) => hex.match(/.{2}/g).reverse().join('');
+    return parseInt(reverseHex(hex), 16);
+}
+
+function str2int(str:string):number{
+    return parseInt(str);
+}
+
+function int2str(int:number):string{
+    return JSON.stringify(int);
 }
 
 export default neoEventHandler;
