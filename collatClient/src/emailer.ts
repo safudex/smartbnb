07d70c86953716441644b3fab6b01ad1c0f7a9b5
Albert <github@albert.sh>
2020-05-21 17:22:56 +0200

    Fix emailer compilation

--- collatClient/src/emailer.ts
+++ collatClient/src/emailer.ts
@@ -1,25 +1,28 @@
 import nodemailer from 'nodemailer';
 
 export default class Emailer {
-    private transporter:any;
+    private transporter: Promise<nodemailer.Transporter>;
 
-    constructor(private toEmailAddress:string) {
+    constructor(private toEmailAddress: string) {
       // Use ethereal.email
-      nodemailer.createTestAccount().then((account) => {
-        this.transporter = nodemailer.createTransport({
-          host: 'smtp.ethereal.email',
-          port: 587,
-          secure: false, // true for 465, false for other ports
-          auth: {
-            user: account.user, // generated ethereal user
-            pass: account.pass, // generated ethereal password
-          },
-        });
+      this.transporter = new Promise((resolve, reject) => {
+        nodemailer.createTestAccount().then((account) => {
+          const transp = nodemailer.createTransport({
+            host: 'smtp.ethereal.email',
+            port: 587,
+            secure: false, // true for 465, false for other ports
+            auth: {
+              user: account.user, // generated ethereal user
+              pass: account.pass, // generated ethereal password
+            },
+          });
+          return resolve(transp);
+        }).catch(reject);
       });
     }
 
-    public async send(message:string) {
-      await this.transporter.sendMail({
+    public async send(message: string) {
+      await (await this.transporter).sendMail({
         from: '"Collateral provider" <collateral@smartbnb.net>',
         to: this.toEmailAddress,
         subject: 'Updates on your collateral provider',
