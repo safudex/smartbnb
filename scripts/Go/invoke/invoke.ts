cec44c38ca529b41fa11d2d939c67fab1cf07f06
Salah Karim <salahkarimka@gmail.com>
2020-05-21 04:52:03 +0200

    add types + reorg

--- scripts/Go/invoke/invoke.ts
+++ scripts/Go/invoke/invoke.ts
@@ -4,16 +4,34 @@ const Neon = require("@cityofzion/neon-js");
 
 const CONSTRACT_HASH = "c1384e12e410cc6a58677324b5ac14ad255684e4" //const
 
-// CONTRACT CONSTANTS
-const SLICESLEN = 16
-const STG_TYPE_GENERAL = "GENERAL";
-const STG_TYPE_PM = "PM";
-const STG_TYPE_POINTMUL_SIMPLE = "SIMPLE";
-const STG_TYPE_POINTMUL_MULTI = "MULTI";
-const STG_TYPE_SIGNABLEBYTES = "SIGNABLEBYTES";
+enum CONTRACT_CONSTANTS {
+    SLICESLEN = 16,
+    STG_TYPE_GENERAL = "GENERAL",
+    STG_TYPE_PM = "PM",
+    STG_TYPE_POINTMUL_SIMPLE = "SIMPLE",
+    STG_TYPE_POINTMUL_MULTI = "MULTI",
+    STG_TYPE_SIGNABLEBYTES = "SIGNABLEBYTES",
+    OPERATION_SAVE = "savestate"
+}
 
-async function getTxResult(txid, leftAttemps){
-    let txres = await axios.post(node, { jsonrpc: "2.0", id: 5, method: "getapplicationlog", params: [txid] })
+enum ARGS_TYPES { String = "String", Array = "Array", ByteArray = "ByteArray", address = "address", fixed8 = "fixed8", Integer = "Integer"}
+
+type rawstate = {
+    signatures:string,
+    xs:string,
+    ys:string,
+    signableBytes:string,
+    pres:string,
+    presHash:string,
+    presHashMod:string,
+    txProof:string,
+    blockHeader:string,
+    txBytes:string
+}
+
+
+async function getTxResult(txid: string, leftAttemps: number){
+    let txres = await axios.post(NODE_URL, { jsonrpc: "2.0", id: 5, method: "getapplicationlog", params: [txid] })
     if (txres.data.error && leftAttemps > 0 ) {
         console.log(leftAttemps, txid, txres.data.error)
         sleep(1000)
@@ -27,28 +45,16 @@ async function getTxResult(txid, leftAttemps){
     return txres
 }
 
-async function invokeOperation(operation, args, gas, fees, sh=CONSTRACT_HASH){
-
-    let txr
+async function invokeOperation(operation: string, args: any[], gas: number, fees: number, sh: string = CONSTRACT_HASH) {
     try {
         const response = await invoke(operation, args, gas, fees, sh)
-        txr = (await getTxResult(response.txid, 2*60)).data.result//.executions[0]
-        if (!txr) {
-            console.log("Attemps exhausted")
-            //retry
-        }
-        else{
-            console.log(txr.executions[0])//return txr
-            txr.executions[0].notifications.forEach(el => {
-                console.log(el.state.value[0].value)
-            });
-        }
+        console.log(response.txid)
     } catch (error) {
         console.log(error.message)
     }
 }
 
-async function invoke(operation, args, gas, fees, sh){
+async function invoke(operation: string, args: any[], gas: number, fees: number, sh:string) {
     return Neon.default.doInvoke({
         api: API_NODE, // The API Provider that we rely on for balance and rpc information
         account: ECO_WALLET, // The sending Account
@@ -58,31 +64,17 @@ async function invoke(operation, args, gas, fees, sh){
     }).then(res => {return res.response})
 }
 
-function pushParams(neonJSParams, type, value, ) {
-    if (type == 'String')
+function pushParams(neonJSParams: any[], type: ARGS_TYPES, value: string | number | any[]) {
+    if (type == 'String' || type == 'Array' || type == 'ByteArray')
         neonJSParams.push(Neon.default.create.contractParam(type, value));
-    else if (type == 'Address')
-        neonJSParams.push(Neon.sc.ContractParam.byteArray(value, 'address'));
-    else if (type == 'Hex')
-        neonJSParams.push(Neon.default.create.contractParam('ByteArray', value));
-    else if (type == 'DecFixed8')
+    else if (type == 'address' || type == 'fixed8')
         // Decimal fixed 8 seems to break at transition 92233720368.54775807 -> 92233720368.54775808
-        neonJSParams.push(Neon.sc.ContractParam.byteArray(value, 'fixed8'))
+        neonJSParams.push(Neon.sc.ContractParam.byteArray(value, type));
 	else if (type == 'Integer')
         neonJSParams.push(Neon.sc.ContractParam.integer(value));
-    else if (type == 'Array')
-        neonJSParams.push(Neon.default.create.contractParam(type, value));
 }
 
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-function randomInt(low, high) {
-  return Math.floor(Math.random() * (high - low) + low)
-}
-
-function arrayTo2DArray1(list, howMany) {
+function arrayTo2DArray1(list: any[], howMany: number) {
     var result = []
     var input = list.slice(0)
     while (input[0]) {
@@ -91,78 +83,82 @@ function arrayTo2DArray1(list, howMany) {
     return result
 }
 
-async function SaveState(raw_state){
+function sleep(ms: number) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function SaveState(raw_state: rawstate){
         var neonJSParams = [];
         
 		//0 byte[] porting
-        pushParams(neonJSParams, 'Hex', PORTINGCONTRACTID)
+        pushParams(neonJSParams, ARGS_TYPES.ByteArray, PORTINGCONTRACTID)
 
         //1 string type
-		pushParams(neonJSParams, 'String', STG_TYPE_GENERAL)
+		pushParams(neonJSParams, ARGS_TYPES.String, CONTRACT_CONSTANTS.STG_TYPE_GENERAL)
         
         //2 ulong[][] pre
         var pres = raw_state.pres.split(" ").map(v => v.split(",").map(Neon.sc.ContractParam.integer))
-        pushParams(neonJSParams, 'Array', pres);
+        pushParams(neonJSParams, ARGS_TYPES.Array, pres);
         
 		//3 ulong[][] preshash
         var presHash = raw_state.presHash.split(" ").map(v => v.split(",").map(Neon.sc.ContractParam.integer))
-		pushParams(neonJSParams, 'Array', presHash);
+		pushParams(neonJSParams, ARGS_TYPES.Array, presHash);
         
         //4 byte[] txproof
-        pushParams(neonJSParams, 'Hex', raw_state.txProof)
+        pushParams(neonJSParams, ARGS_TYPES.ByteArray, raw_state.txProof)
         
 		//5 byte[] blockHeader
-        pushParams(neonJSParams, 'Hex', raw_state.blockHeader);
+        pushParams(neonJSParams, ARGS_TYPES.ByteArray, raw_state.blockHeader);
 
 		//6 ulong[] txBytes
         var txBytes = raw_state.txBytes.split(",").map(Neon.sc.ContractParam.integer)
-		pushParams(neonJSParams, 'Array', txBytes);
+		pushParams(neonJSParams, ARGS_TYPES.Array, txBytes);
 
-        await invokeOperation("savestate", neonJSParams, 50, 10)
+        await invokeOperation(CONTRACT_CONSTANTS.OPERATION_SAVE, neonJSParams, 50, 10)
 
         neonJSParams = []	
         
 		//0 byte[] porting
-        pushParams(neonJSParams, 'Hex', PORTINGCONTRACTID)
+        pushParams(neonJSParams, ARGS_TYPES.ByteArray, PORTINGCONTRACTID)
 
         //1 string type
-        pushParams(neonJSParams, 'String', STG_TYPE_PM)
+        pushParams(neonJSParams, ARGS_TYPES.String, CONTRACT_CONSTANTS.STG_TYPE_PM)
 
         //2 byte[][] signatures
 		var signatures = raw_state.signatures.split(",").map(v => Neon.default.create.contractParam('ByteArray', v))
-		pushParams(neonJSParams, 'Array', signatures);
+		pushParams(neonJSParams, ARGS_TYPES.Array, signatures);
         
         //3 BigInteger[] xs
 		var xs = raw_state.xs.split(",").map(Neon.sc.ContractParam.integer)
-        pushParams(neonJSParams, 'Array', xs);
+        pushParams(neonJSParams, ARGS_TYPES.Array, xs);
         
 		//4 BigInteger[] ys
 		var ys = raw_state.ys.split(",").map(Neon.sc.ContractParam.integer)
-        pushParams(neonJSParams, 'Array', ys);
+        pushParams(neonJSParams, ARGS_TYPES.Array, ys);
         
         //5 BigInteger[] presHashMod
 		var presHashMod = raw_state.presHashMod.split(",").map(Neon.sc.ContractParam.integer)
-		pushParams(neonJSParams, 'Array', presHashMod);
+		pushParams(neonJSParams, ARGS_TYPES.Array, presHashMod);
 
-        await invokeOperation("savestate", neonJSParams, 500, 100)
+        await invokeOperation(CONTRACT_CONSTANTS.OPERATION_SAVE, neonJSParams, 500, 100)
 
         //SENDING SIGNABLEBYTES IN CHUNKS
         var signableBytes = raw_state.signableBytes.split(",").map(v=>v.match(/.{1,2}/g).map(v=>Neon.sc.ContractParam.integer(parseInt(v, 16))))
         signableBytes.forEach(async (signBytes, index) => {
             neonJSParams = []
             //0 byte[] porting
-            pushParams(neonJSParams, 'Hex', PORTINGCONTRACTID)
+            pushParams(neonJSParams, ARGS_TYPES.ByteArray, PORTINGCONTRACTID)
             //1 string type
-            pushParams(neonJSParams, 'String', STG_TYPE_SIGNABLEBYTES)
+            pushParams(neonJSParams, ARGS_TYPES.String, CONTRACT_CONSTANTS.STG_TYPE_SIGNABLEBYTES)
             //2 string id
-            pushParams(neonJSParams, 'String', ""+String.fromCharCode(index))
+            pushParams(neonJSParams, ARGS_TYPES.String, ""+String.fromCharCode(index))
             //3 ulong[] data
-            pushParams(neonJSParams, 'Array', signBytes)
-            await invokeOperation("savestate", neonJSParams, 500, 100)
+            pushParams(neonJSParams, ARGS_TYPES.Array, signBytes)
+            await invokeOperation(CONTRACT_CONSTANTS.OPERATION_SAVE, neonJSParams, 500, 100)
         })
 
         //SENDING POINTMUL IN CHUNKS
-        const numSlices = 256/SLICESLEN
+        const numSlices = 256/CONTRACT_CONSTANTS.SLICESLEN
         var pointMulData=fs.readFileSync('pointmulsteps', 'utf-8').split("||");
         
         //point mul sb
@@ -179,9 +175,9 @@ async function SaveState(raw_state){
 			Ps_sb = Ps_sb.concat(arrayTo2DArray1(Ps_tmp, 4))
 		})
 
-		await savePointMuls(Ps_sb, numSlices, "Ps_sb", STG_TYPE_POINTMUL_MULTI)
-		await savePointMuls(ss_sb, numSlices, "ss_sb", STG_TYPE_POINTMUL_SIMPLE)
-		await savePointMuls(Qs_sb, numSlices, "Qs_sb", STG_TYPE_POINTMUL_MULTI)
+		await savePointMuls(Ps_sb, numSlices, "Ps_sb", CONTRACT_CONSTANTS.STG_TYPE_POINTMUL_MULTI)
+		await savePointMuls(ss_sb, numSlices, "ss_sb", CONTRACT_CONSTANTS.STG_TYPE_POINTMUL_SIMPLE)
+		await savePointMuls(Qs_sb, numSlices, "Qs_sb", CONTRACT_CONSTANTS.STG_TYPE_POINTMUL_MULTI)
 
         //point mul ha
 		var ss_ha = []
@@ -197,30 +193,31 @@ async function SaveState(raw_state){
 			Ps_ha = Ps_ha.concat(arrayTo2DArray1(Ps_tmp, 4))
 		})
 
-        await savePointMuls(Ps_ha, numSlices, "Ps_ha", STG_TYPE_POINTMUL_MULTI)
-		await savePointMuls(ss_ha, numSlices, "ss_ha", STG_TYPE_POINTMUL_SIMPLE)
-		await savePointMuls(Qs_ha, numSlices, "Qs_ha", STG_TYPE_POINTMUL_MULTI)
+        await savePointMuls(Ps_ha, numSlices, "Ps_ha", CONTRACT_CONSTANTS.STG_TYPE_POINTMUL_MULTI)
+		await savePointMuls(ss_ha, numSlices, "ss_ha", CONTRACT_CONSTANTS.STG_TYPE_POINTMUL_SIMPLE)
+		await savePointMuls(Qs_ha, numSlices, "Qs_ha", CONTRACT_CONSTANTS.STG_TYPE_POINTMUL_MULTI)
 }
 
-async function savePointMuls(arr, nchks, id, type) {
+async function savePointMuls(arr: any[], nchks: number, id: string, type: string) {
     var j = arr.length/nchks
     for (var i=0; i<nchks; i++) {
         var neonJSParams = []
         //0 byte[] porting
-        pushParams(neonJSParams, 'Hex', PORTINGCONTRACTID)
+        pushParams(neonJSParams, ARGS_TYPES.ByteArray, PORTINGCONTRACTID)
         //1 string type
-        pushParams(neonJSParams, 'String', type)
+        pushParams(neonJSParams, ARGS_TYPES.String, type)
         //2 string id
-        pushParams(neonJSParams, 'String', id+String.fromCharCode(i))
+        pushParams(neonJSParams, ARGS_TYPES.String, id+String.fromCharCode(i))
         //3 string data
-        pushParams(neonJSParams, 'Array', arr.slice(i*j, (i+1)*j))
-        await invokeOperation("savestate", neonJSParams, 50, 10)
+        pushParams(neonJSParams, ARGS_TYPES.Array, arr.slice(i*j, (i+1)*j))
+        await invokeOperation(CONTRACT_CONSTANTS.OPERATION_SAVE, neonJSParams, 50, 10)
     }
 }
 
-const cmdArgs = process.argv.slice(2)
+const cmdArgs: string[] = process.argv.slice(2)
 var ECO_WALLET = new Neon.wallet.Account(cmdArgs[0]); //ALWAYS CONST????
-const API_NODE = new Neon.api.neoCli.instance(cmdArgs[1]);
+const NODE_URL = cmdArgs[1]
+const API_NODE = new Neon.api.neoCli.instance(NODE_URL);
 const PORTINGCONTRACTID = cmdArgs[2]
 
 const raw_state = {
@@ -233,7 +230,7 @@ const raw_state = {
     presHashMod:cmdArgs[9],
     txProof:cmdArgs[10],
     blockHeader:cmdArgs[11],
-    txBytes:cmdArgs[12],
+    txBytes:cmdArgs[12]
 }
 
 SaveState(raw_state)
